fd542b22ec1698561aab95e43bf3d8fe04a5f78d
angussidney <4848802+angussidney@users.noreply.github.com>
2018-01-20 22:06:33 +1100

    Complete major components of notifications script

--- github-notifications-opener/github-notifications-opener.user.js
+++ github-notifications-opener/github-notifications-opener.user.js
@@ -7,13 +7,38 @@
 // @updateURL   https://raw.githubusercontent.com/angussidney/userscripts/master/github-notifications-opener/github-notifications-opener.user.js
 // @downloadURL https://raw.githubusercontent.com/angussidney/userscripts/master/github-notifications-opener/github-notifications-opener.user.js
 // @supportURL  https://github.com/angussidney/userscripts/issues
-// @match       *://github.com/notifications*
+// @match       *://github.com/notifications
 // @grant       none
 // ==/UserScript==
 
 (() => {
+  function openAllNotifications() {
+    repos = document.getElementsByClassName('notifications')
+    for (repo in repos) {
+      notifications = repos[repo]
+      for (n in notifications) {
+        notification = notifications[n]
+        if (notification.classList.contains('js-notification')) {
+          notificationURL = notification.querySelector('a.list-group-item-link').href
+          // window.open(notificationURL)
+          console.log(notificationURL)
+        }
+      }
+    }
+    // Refresh the page to clear notifications
+    // window.location.reload()
+  }
+
   // Init: Add button
-  // Click handler
-  // Open URLs
-  // Remove button
-})();
+  button = document.createElement('a')
+  button.className = 'btn btn-sm'
+  button.id = 'open-notifications'
+  button.textContent = 'Open all in new tabs'
+  navArea = document.querySelector('.tabnav > .float-right')
+  navArea.prepend(button)
+
+  // Button click handling
+  document.getElementById('open-notifications').onclick = openAllNotifications
+
+  // TODO: Only add button if notifications present
+})()
