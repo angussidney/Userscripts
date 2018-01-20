6e5f034ebda10bc42acead394aab917b562d55c6
angussidney <4848802+angussidney@users.noreply.github.com>
2018-01-20 20:00:40 +1100

    Add notifications US, update readme+license

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# userscripts
-A collection of userscripts that I use
+# Userscripts
+
+These scripts will require a userscript extension in order to run. I suggest using
+[ViolentMonkey](https://addons.mozilla.org/en-GB/firefox/addon/violentmonkey/) for Firefox 57+ and
+[TamperMonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) for Chrome.
+
+## Downloads
+
+ - [Github Notifications Opener](https://raw.githubusercontent.com/angussidney/userscripts/master/github-notifications-opener/github-notifications-opener.user.js) ([source](https://github.com/angussidney/userscripts/blob/master/github-notifications-opener/github-notifications-opener.user.js))
---
+++ github-notifications-opener/github-notifications-opener.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Github Notifications Opener
+// @namespace   https://github.com/angussidney
+// @description Adds a button to the notifications page which opens all visible notifications
+// @author      angussidney
+// @version     0.1
+// @updateURL   https://raw.githubusercontent.com/angussidney/userscripts/master/github-notifications-opener/github-notifications-opener.user.js
+// @downloadURL https://raw.githubusercontent.com/angussidney/userscripts/master/github-notifications-opener/github-notifications-opener.user.js
+// @supportURL  https://github.com/angussidney/userscripts/issues
+// @match       *://github.com/notifications*
+// @grant       none
+// ==/UserScript==
+
+(() => {
+  // Init: Add button
+  // Click handler
+  // Open URLs
+  // Remove button
+})();
